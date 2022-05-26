8125b7dae309d88a19e2cf1c374d33d21ea82f67
holliwid <stepales1111@gmail.com>
2022-05-27 02:22:27 +0300

    crm

---
+++ contracts.py
@@ -0,0 +1,166 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import main
+
+
+import sys
+import psycopg2
+from PyQt5.QtWidgets import QTableWidget, QApplication, QMainWindow, QTableWidget
+from PyQt5.QtWidgets import QTableWidgetItem, QWidget, QPushButton, QLineEdit
+from PyQt5 import QtGui
+from PyQt5.QtCore import pyqtSlot
+
+
+class Raice(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.con()
+        self.w = None
+        # подключить базу данных
+        self.con()
+        # параметры окна
+        self.setGeometry(100, 100, 1000, 800)
+        self.setWindowTitle('Рейсы')
+        self.tb = Tb(self)
+        # кнопка "обновить"
+        self.btn = QPushButton('Обновить', self)
+        self.btn.resize(150, 40)
+        self.btn.move(300, 10)
+        self.btn.clicked.connect(self.upd)
+        # здесь идентификатор
+        self.idp = QLineEdit(self)
+        self.idp.resize(150, 40)
+        self.idp.move(300, 60)
+        # здесь тип машины
+        self.type = QLineEdit(self)
+        self.type.resize(150, 40)
+        self.type.move(300, 110)
+        # здесь изображение
+        self.img = QLineEdit(self)
+        self.img.resize(150, 40)
+        self.img.move(300, 160)
+        # здесь номер машины
+        self.num = QLineEdit(self)
+        self.num.resize(150, 40)
+        self.num.move(300, 210)
+        # кнопка добавить запись
+        self.btn = QPushButton('Добавить', self)
+        self.btn.resize(150, 40)
+        self.btn.move(300, 260)
+        self.btn.clicked.connect(self.ins)
+        # кнопка удалить запись
+        self.btn = QPushButton('Удалить', self)
+        self.btn.resize(150, 40)
+        self.btn.move(300, 310)
+        self.btn.clicked.connect(self.dels)
+
+        self.btn = QPushButton(f'Меню', self)
+        self.btn.setGeometry(30, self.height() - 100, 100, 50)
+        self.btn.clicked.connect(self.to_menu)
+
+
+
+
+    # соединение с базой данных
+
+
+    # обновить таблицу и поля
+    def upd(self):
+        self.conn.commit()
+        self.tb.updt()
+        self.idp.setText('')
+        self.type.setText('')
+        self.img.setText('')
+        self.num.setText('')
+
+
+    # добавить таблицу новую строку
+    def ins(self):
+        idp, type, img, num = self.idp.text(), self.type.text(), self.img.text(), self.num.text()
+        try:
+            self.cur.execute("insert into cars (car_id, cars_type_id, img, car_number) values (%s,%s,%s,%s)", (int(idp), int(type), img, num))
+        except:
+            print('error')
+        self.upd()
+
+    # удалить из таблицы строку
+    def dels(self):
+        try:
+            ids = int(self.idp.text())  # идентификатор строки
+        except:
+            return
+        self.cur.execute("delete from cars where car_id=%s", (ids,))
+        self.conn.commit()
+        self.upd()
+
+
+    def con(self):
+        self.conn = psycopg2.connect(user="postgres",
+                                     password="123",
+                                     host="127.0.0.1",
+                                     port="5432",
+                                     database="postgres")
+        self.cur = self.conn.cursor()
+
+
+
+
+
+    @pyqtSlot()
+    def to_menu(self):
+        self.window().close()
+        if self.w is None:
+            self.w = main.Menu()
+            self.w.show()
+
+        else:
+            self.w.close()  # Close window.
+            self.w = None  # Discard reference.
+
+    @pyqtSlot()
+    def on_click_drivers(self):
+        print('drivers')
+
+    @pyqtSlot()
+    def on_click_raice(self):
+        print('raice')
+
+
+
+class Tb(QTableWidget):
+    def __init__(self, wg):
+        self.wg = wg  # запомнить окно, в котором эта таблица показывается
+        super().__init__(wg)
+        self.setGeometry(10, 10, 280, 500)
+        self.setColumnCount(4)
+        self.verticalHeader().hide();
+        self.updt() # обновить таблицу
+        self.setEditTriggers(QTableWidget.NoEditTriggers) # запретить изменять поля
+        self.cellClicked.connect(self.cellClick)  # установить обработчик щелча мыши в таблице
+
+# обновление таблицы
+    def updt(self):
+        self.clear()
+        self.setRowCount(0);
+        self.setHorizontalHeaderLabels(['car_id', 'cars_type_id', 'img', 'car_num']) # заголовки столцов
+        self.wg.cur.execute("select * from cars")
+        rows = self.wg.cur.fetchall()
+        print(rows)
+        i = 0
+        for elem in rows:
+            self.setRowCount(self.rowCount() + 1)
+            j = 0
+            for t in elem: # заполняем внутри строки
+                self.setItem(i, j, QTableWidgetItem(str(t).strip()))
+                j += 1
+            i += 1
+        self.resizeColumnsToContents()
+
+# обработка щелчка мыши по таблице
+    def cellClick(self, row, col): # row - номер строки, col - номер столбца
+        self.wg.idp.setText(self.item(row, 0).text())
+        self.wg.type.setText(self.item(row, 1).text().strip())
+        self.wg.img.setText(self.item(row, 2).text().strip())
+        self.wg.num.setText(self.item(row, 3).text().strip())
